45fcfbd7e524be21f24dbff03c2739ca51916fec
Evan Reinstein <42251756+EvanReinstein@users.noreply.github.com>
2025-03-19 11:14:52 -0500

    add button to return to 'checkout' page

--- client/one-time-payment/react/src/pages/failurePage.tsx
+++ client/one-time-payment/react/src/pages/failurePage.tsx
@@ -3,7 +3,8 @@ import React from "react";
 export const FailurePage: React.FC = () => {
   return (
     <>
-    <h1>Oh no! Something went wrong.</h1>
+      <h1>Oh no! Something went wrong.</h1>
+      <button type="button" onClick={() => window.location.replace("/")} >Return to Checkout</button>
     </>
-  )
+  );
 };
