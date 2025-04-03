6ef5c0e67c53306914eecf2d293bcb512b48b955
Hamza Nasir <munasir@paypal.com>
2025-04-03 10:45:42 -0500

    change default for clientToken

--- client/one-time-payment/react/src/App.tsx
+++ client/one-time-payment/react/src/App.tsx
@@ -25,7 +25,7 @@ function ErrorFallback({ error }: { error: Error }) {
 }
 
 function App() {
-  const [clientToken, setClientToken] = useState<string | undefined>(undefined);
+  const [clientToken, setClientToken] = useState<string>('');
 
   useEffect(() => {
     const getClientToken = async () => {
