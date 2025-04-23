9f9d547249cb83faa767d15ea14baf3549d72cc5
Evan Reinstein <42251756+EvanReinstein@users.noreply.github.com>
2025-04-23 14:09:31 -0500

    update buttons to prevent re-render unless sdk instance changes

--- client/one-time-payment/react/src/components/VenmoButton.tsx
+++ client/one-time-payment/react/src/components/VenmoButton.tsx
@@ -1,21 +1,29 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useRef } from "react";
 import { PayPalSDKContext } from "../context/sdkContext";
 import { createOrder } from "../utils";
-import { PaymentSessionOptions } from "../types/paypal";
+import { PaymentSessionOptions, SessionOutput } from "../types/paypal";
 import { useErrorBoundary } from "react-error-boundary";
 
 const VenmoButton: React.FC<PaymentSessionOptions> = (
   paymentSessionOptions,
 ) => {
   const { sdkInstance } = useContext(PayPalSDKContext);
-  const venmoSession = sdkInstance!.createVenmoOneTimePaymentSession(
-    paymentSessionOptions,
-  );
   const { showBoundary } = useErrorBoundary();
+  const venmoSession = useRef<SessionOutput>(null);
+
+  useEffect(() => {
+    if (sdkInstance) {
+      venmoSession.current = sdkInstance.createVenmoOneTimePaymentSession(
+        paymentSessionOptions
+      );
+    }
+  }, [sdkInstance]);
 
   const venmoOnClickHandler = async () => {
+    if (!venmoSession.current) return;
+    
     try {
-      await venmoSession?.start({ presentationMode: "auto" }, createOrder());
+      await venmoSession.current.start({ presentationMode: "auto" }, createOrder());
     } catch (e) {
       console.error(e);
       showBoundary(e);
