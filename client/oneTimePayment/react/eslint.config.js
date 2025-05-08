40b5de3955e3ca920fc795e0fc67c80061257ace
Greg Jopa <534034+gregjopa@users.noreply.github.com>
2025-05-08 15:12:49 -0500

    Add rule for no extensions

--- client/oneTimePayment/react/eslint.config.js
+++ client/oneTimePayment/react/eslint.config.js
@@ -25,6 +25,7 @@ export default tseslint.config(
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
+      "import-x/extensions": ["error", "never"],
       "react-refresh/only-export-components": [
         "warn",
         { allowConstantExport: true },
