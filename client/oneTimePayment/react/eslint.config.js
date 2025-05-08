0c5908897d51be52e6e6d9a56fe55658f9a491ad
Greg Jopa <534034+gregjopa@users.noreply.github.com>
2025-05-08 15:06:15 -0500

    chore: lint imports

--- client/oneTimePayment/react/eslint.config.js
+++ client/oneTimePayment/react/eslint.config.js
@@ -1,17 +1,23 @@
 import js from "@eslint/js";
-import globals from "globals";
+import * as pluginImportX from "eslint-plugin-import-x";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
 import tseslint from "typescript-eslint";
+import tsParser from "@typescript-eslint/parser";
+import * as tsResolver from "eslint-import-resolver-typescript";
 
 export default tseslint.config(
+  js.configs.recommended,
+  tseslint.configs.recommended,
+  pluginImportX.flatConfigs.recommended,
+  pluginImportX.flatConfigs.typescript,
   { ignores: ["dist"] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
     files: ["**/*.{ts,tsx}"],
     languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
+      parser: tsParser,
+      ecmaVersion: "latest",
+      sourceType: "module",
     },
     plugins: {
       "react-hooks": reactHooks,
@@ -23,6 +29,12 @@ export default tseslint.config(
         "warn",
         { allowConstantExport: true },
       ],
+    },
+    settings: {
+      "import-x/resolver": {
+        name: "tsResolver",
+        resolver: tsResolver,
+      },
     },
   },
 );
