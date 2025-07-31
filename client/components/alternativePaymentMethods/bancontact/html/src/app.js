6ef667ddc6dc80cd6fced14bb974f57147f992ac
Nate Bierdeman <nbierdeman@paypal.com>
2025-07-31 12:25:30 -0400

    add v6 paypal button

---
+++ client/components/alternativePaymentMethods/bancontact/html/src/app.js
@@ -0,0 +1,83 @@
+async function onV6PayPalWebSdkLoaded() {
+  try {
+    const clientToken = await getBrowserSafeClientToken();
+    const sdkInstance = await window.paypal.createInstance({
+      clientToken,
+      components: ["paypal-payments"],
+      pageType: "checkout",
+    });
+
+    const paypalPaymentSession = sdkInstance.createPayPalOneTimePaymentSession(
+      paymentSessionOptions,
+    );
+
+    const paypalButton = document.querySelector("#v6-paypal-button");
+    paypalButton.removeAttribute("hidden");
+
+    paypalButton.addEventListener("click", async () => {
+      await paypalPaymentSession.start(
+        { presentationMode: "auto" },
+        paymentSessionOptions.createOrder(),
+      );
+    });
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+const paymentSessionOptions = {
+  async onApprove(data) {
+    console.log("onApprove", data);
+    const orderData = await captureOrder({
+      orderId: data.orderId,
+    });
+    console.log("Capture result", orderData);
+  },
+  async createOrder() {
+    const response = await fetch(
+      "/paypal-api/checkout/orders/create-with-sample-data",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      },
+    );
+    const { id } = await response.json();
+
+    return { orderId: id };
+  },
+  onCancel(data) {
+    console.log("onCancel", data);
+  },
+  onError(error) {
+    console.log("onError", error);
+  },
+};
+
+async function getBrowserSafeClientToken() {
+  const response = await fetch("/paypal-api/auth/browser-safe-client-token", {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  const { accessToken } = await response.json();
+
+  return accessToken;
+}
+
+async function captureOrder({ orderId }) {
+  const response = await fetch(
+    `/paypal-api/checkout/orders/${orderId}/capture`,
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    },
+  );
+  const data = await response.json();
+
+  return data;
+}
