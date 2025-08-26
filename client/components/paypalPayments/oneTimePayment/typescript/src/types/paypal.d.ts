4df7fe3ba16629944d389c588a18b83f023e750c
Evan Reinstein <42251756+EvanReinstein@users.noreply.github.com>
2025-08-26 11:30:52 -0500

    update global paypal object typing

--- client/components/paypalPayments/oneTimePayment/typescript/src/types/paypal.d.ts
+++ client/components/paypalPayments/oneTimePayment/typescript/src/types/paypal.d.ts
@@ -1,12 +1,8 @@
-import { SdkInstance, CreateInstanceOptions } from "@paypal/paypal-js/sdk-v6";
+import type { PayPalV6Namespace, SdkInstance, CreateInstanceOptions } from "@paypal/paypal-js/sdk-v6";
 
 declare global {
   interface Window {
-    paypal: {
-      createInstance: (
-        createInstanceOptions: CreateInstanceOptions,
-      ) => Promise<SdkInstance>;
-    };
+    paypal: PayPalV6Namespace;
   }
 }
 
