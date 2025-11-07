c2a6b8a00b5306d908d56fda86bd6a9d505074ec
Nate Bierdeman <nbierdeman@paypal.com>
2025-11-07 10:46:09 -0500

    rename const

--- client/components/paypalPayments/oneTimePayment/html/src/advanced/redirect/app.js
+++ client/components/paypalPayments/oneTimePayment/html/src/advanced/redirect/app.js
@@ -42,7 +42,7 @@ async function setupPayPalButton(paypalPaymentSession) {
   paypalButton.removeAttribute("hidden");
 
   paypalButton.addEventListener("click", async () => {
-    const createOrderPromise = createRedirectOrder();
+    const createRedirectOrderPromise = createRedirectOrder();
 
     try {
       const { redirectURL } = await paypalPaymentSession.start(
@@ -52,7 +52,7 @@ async function setupPayPalButton(paypalPaymentSession) {
             enabled: enableAutoRedirect.checked,
           },
         },
-        createOrderPromise,
+        createRedirectOrderPromise,
       );
       if (redirectURL) {
         console.log(`redirectURL: ${redirectURL}`);
