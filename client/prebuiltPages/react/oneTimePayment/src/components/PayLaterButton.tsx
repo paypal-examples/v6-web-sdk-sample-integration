d38e8913dcf6527615ca76c14870e99ed807e6db
akos_paypal <akos@paypal.com>
2025-12-03 08:40:03 -0600

    wip

--- client/prebuiltPages/react/oneTimePayment/src/components/PayLaterButton.tsx
+++ client/prebuiltPages/react/oneTimePayment/src/components/PayLaterButton.tsx
@@ -1,10 +1,8 @@
-import React, { useRef, useEffect } from "react";
+import React from "react";
 
 import { createOrder } from "../utils";
-import { useErrorBoundary } from "react-error-boundary";
-import { usePayPal } from "@paypal/react-paypal-js/sdk-v6";
+import { usePayLaterOneTimePaymentSession } from "@paypal/react-paypal-js/sdk-v6";
 import type {
-  OneTimePaymentSession,
   PayLaterCountryCodes,
   PayLaterOneTimePaymentSessionOptions,
   PayLaterProductCodes,
@@ -24,37 +22,17 @@ const PayLaterButton: React.FC<PayLaterButtonProps> = (
     ...paymentSessionOptions
   },
 ) => {
-  const { sdkInstance } = usePayPal();
-  const { showBoundary } = useErrorBoundary();
-  const payLaterSession = useRef<OneTimePaymentSession>(null);
-
-  useEffect(() => {
-    if (sdkInstance) {
-      payLaterSession.current = sdkInstance.createPayLaterOneTimePaymentSession(
-        paymentSessionOptions,
-      );
-    }
-  }, [sdkInstance, paymentSessionOptions]);
-
-  const payPalOnClickHandler = async () => {
-    if (!payLaterSession.current) return;
-
-    try {
-      await payLaterSession.current.start(
-        { presentationMode: "auto" },
-        createOrder(),
-      );
-    } catch (e) {
-      console.error(e);
-      showBoundary(e);
-    }
-  };
+  const { error, handleClick } = usePayLaterOneTimePaymentSession({
+    presentationMode: "auto",
+    onApprove: async () => console.log("Pay Later payment approved"),
+    createOrder,
+  })
 
   return (
     <paypal-pay-later-button
       countryCode={countryCode}
       id="paypal-button"
-      onClick={() => payPalOnClickHandler()}
+      onClick={handleClick}
       productCode={productCode}
       type="pay"
     ></paypal-pay-later-button>
--- client/prebuiltPages/react/oneTimePayment/src/main.tsx
+++ client/prebuiltPages/react/oneTimePayment/src/main.tsx
@@ -2,3 +2,6 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 
 createRoot(document.getElementById("root")!).render(<App />);
+
+//import { ErrorExamples } from './ErrorExamples';
+//createRoot(document.getElementById("root")!).render(<ErrorExamples />);
