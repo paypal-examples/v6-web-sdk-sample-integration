a84ffcc99364688959d0d5b8cb90cf67407bc1d8
Greg Jopa <534034+gregjopa@users.noreply.github.com>
2026-02-06 16:20:22 -0600

    fix: only import ngrok if its needed

--- server/node/src/server.ts
+++ server/node/src/server.ts
@@ -2,7 +2,6 @@ import express, { Request, Response } from "express";
 import { join } from "path";
 import { z } from "zod/v4";
 import cors from "cors";
-import ngrok from "@ngrok/ngrok";
 
 import type {
   PaymentTokenResponse,
@@ -287,6 +286,8 @@ async function setupNgrokForHTTPS(port: number) {
   if (!NGROK_AUTHTOKEN) {
     return;
   }
+
+  const ngrok = await import("@ngrok/ngrok");
 
   try {
     const listener = await ngrok.connect({
