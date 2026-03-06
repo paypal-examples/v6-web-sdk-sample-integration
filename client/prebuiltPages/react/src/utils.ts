a6ca913e68b07a8e9587068f26783788fb05bd23
Evan Reinstein <42251756+EvanReinstein@users.noreply.github.com>
2026-03-06 11:58:36 -0600

    chore: prettier

--- client/prebuiltPages/react/src/utils.ts
+++ client/prebuiltPages/react/src/utils.ts
@@ -10,7 +10,7 @@ export const getBrowserSafeClientId = async () => {
   const { clientId } = await response.json();
 
   return clientId;
-}
+};
 
 export const fetchProducts = async () => {
   const response = await fetch("/paypal-api/products", {
