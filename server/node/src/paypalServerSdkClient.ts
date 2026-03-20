c57f920dc06d0c5cb40b22d35bada282e2a03cfd
Greg Jopa <534034+gregjopa@users.noreply.github.com>
2026-03-20 10:54:00 -0500

    chore: set log level with env variable (#201)

--- server/node/src/paypalServerSdkClient.ts
+++ server/node/src/paypalServerSdkClient.ts
@@ -1,10 +1,13 @@
 import { Client, Environment, LogLevel } from "@paypal/paypal-server-sdk";
 
-const { PAYPAL_SANDBOX_CLIENT_ID, PAYPAL_SANDBOX_CLIENT_SECRET } = process.env;
+const { PAYPAL_SANDBOX_CLIENT_ID, PAYPAL_SANDBOX_CLIENT_SECRET, LOG_LEVEL } =
+  process.env;
 
 if (!PAYPAL_SANDBOX_CLIENT_ID || !PAYPAL_SANDBOX_CLIENT_SECRET) {
   throw new Error("Missing API credentials");
 }
+
+const { logLevel, logBody, logHeaders } = getLoggingConfiguration(LOG_LEVEL);
 
 export const client = new Client({
   clientCredentialsAuthCredentials: {
@@ -14,12 +17,59 @@ export const client = new Client({
   timeout: 0,
   environment: Environment.Sandbox,
   logging: {
-    logLevel: LogLevel.Info,
+    logLevel,
     logRequest: {
-      logBody: true,
+      logBody,
     },
     logResponse: {
-      logHeaders: true,
+      logHeaders,
     },
   },
 });
+
+function getLoggingConfiguration(logLevel?: string) {
+  switch (logLevel) {
+    case "error": {
+      return {
+        logLevel: LogLevel.Error,
+        logBody: false,
+        logHeaders: false,
+      };
+    }
+    case "warn": {
+      return {
+        logLevel: LogLevel.Warn,
+        logBody: false,
+        logHeaders: false,
+      };
+    }
+    case "info": {
+      return {
+        logLevel: LogLevel.Info,
+        logBody: true,
+        logHeaders: true,
+      };
+    }
+    case "debug": {
+      return {
+        logLevel: LogLevel.Debug,
+        logBody: true,
+        logHeaders: true,
+      };
+    }
+    case "trace": {
+      return {
+        logLevel: LogLevel.Trace,
+        logBody: true,
+        logHeaders: true,
+      };
+    }
+    default: {
+      return {
+        logLevel: LogLevel.Info,
+        logBody: true,
+        logHeaders: true,
+      };
+    }
+  }
+}
