4746cbf4f0e2936b53493eb23794d43bc8eb717f
nityasp <nitya.pensat@gmail.com>
2026-05-20 16:22:08 -0500

    ApplePay demo (#230)
    * ApplePay demo
    
    * Removing hardcoded domain name
    
    * Loading ApplePay sdk in the script tag
    
    * removing hardcoded clientId and adding paymentFlow to AppleCheckout
    
    * updating the vite config and readme
    
    * running prettier
    
    * updating react-paypal-js version
    
    * fixing build error
    
    * removing the local setup documentation
    
    * addressing comment

--- client/prebuiltPages/react/src/pages/BaseProduct.tsx
+++ client/prebuiltPages/react/src/pages/BaseProduct.tsx
@@ -10,7 +10,7 @@ interface ProductPageProps {
     | "save-payment"
     | "subscription"
     | "vault-with-purchase";
-  paymentMethod?: "card-fields";
+  paymentMethod?: "card-fields" | "apple-pay";
 }
 
 const BaseProduct = ({ flowType, paymentMethod }: ProductPageProps) => {
