1585ecdaf69a51562ca53857cd2a305c175c6929
dtjones404 <dustintaylorjones@gmail.com>
2026-09-17 17:29:40 -0400

    chore: more copilot feedback

--- client/components/paypalPayments/savedPaymentMethods/html/src/app.js
+++ client/components/paypalPayments/savedPaymentMethods/html/src/app.js
@@ -97,7 +97,7 @@ async function configurePayPalButton(sdkInstance) {
   }
   savedPaymentMethodComponent.dataset.listenerAttached = "true";
 
-  const openEditSession = async () => {
+  savedPaymentMethodComponent.addEventListener("click", async () => {
     try {
       // get the promise reference by invoking createOrder()
       // do not await this async function since it can cause transient activation issues
@@ -114,16 +114,6 @@ async function configurePayPalButton(sdkInstance) {
       );
     } catch (error) {
       console.error(error);
-    }
-  };
-
-  savedPaymentMethodComponent.addEventListener("click", openEditSession);
-  // this component isn't a native <button>, so Enter/Space activation
-  // needs to be wired up manually for keyboard users
-  savedPaymentMethodComponent.addEventListener("keydown", (event) => {
-    if (event.key === "Enter" || event.key === " ") {
-      event.preventDefault();
-      openEditSession();
     }
   });
 
